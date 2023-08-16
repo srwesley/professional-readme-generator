10b7b72a1d3019b56ee62d43dc333eff27824117
Sarah Wesley <srwesley722@gmail.com>
2023-08-16 14:58:12 -0500

    Added further to both js files

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,67 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your app?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the app used for?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How do you install the app?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How is the app used?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license did you use:",
+        choices: [
+            "MIT",
+            "Mozilla",
+            "Apache",
+            "Zlib",
+        ]
+    },
+    {
+        type: "input",
+        name: "contributions",
+        message: "How can the user make contributions to the app?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "How do you test the app?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Where can the user email any questions or concerns?"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,47 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+ // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return `[MIT License Badge](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  } else if (license === "Mozilla") {
+    return `[MPL 2.0 License Badge](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)`;
+  } else if (license === "Apache") {
+    return `[Apache License Badge](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+  } else if (license === "Zlib") {
+    return `[Zlib License Badge](https://img.shields.io/badge/License-Zlib-lightgrey.svg)`;
+  } else {
+    return ``;
+  };
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return `[MIT](https://opensource.org/licenses/MIT)`;
+  } else if (license === "Mozilla") {
+    return `[Mozilla](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (license === "Apache") {
+    return `[Apache](https://opensource.org/licenses/Apache=2.0)`;
+  } else if (license === "Zlib") {
+    return `[Zlib](https://opensource.org/licenses/Zlib)`;
+  } else {
+    return ``;
+  };
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "MIT") {
+    return `
+    ${renderLicenseBadge(license)}
+    ${renderLicenseLink(license)}
+    `;
+  } else {
+    "None";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
